0454813ed68e36840cc1be46681bb4d3f12ded0e
Brian Noguchi <brian.noguchi@gmail.com>
2017-03-15 18:54:10 -0700

    Add test.check dep

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :source-paths ["src"]
   :test-paths ["test"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.473"]])
+                 [org.clojure/clojurescript "1.9.473"]
+                 [org.clojure/test.check "0.9.0"]])
