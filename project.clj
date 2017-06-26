4ddfa4e477a0dc02e0ea00f373edd71a9e5666fa
Brian Noguchi <brian.noguchi@gmail.com>
2017-06-26 15:08:23 -0700

    version 0.1.0-alpha11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lab79/clojure-spec-helpers "0.1.0-alpha11-SNAPSHOT"
+(defproject lab79/clojure-spec-helpers "0.1.0-alpha11"
   :description "Helper functions for clojure.spec"
   :url "https://github.com/lab-79/clojure-spec-helpers"
   :license {:name "MIT License"
