b7845dc9347556ede92c6ffbf7a4f0b16105129c
Brian Noguchi <brian.noguchi@gmail.com>
2017-09-26 20:24:19 -0700

    Bump clojure to 1.9.0-beta1

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :source-paths ["src"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/clojurescript "1.9.908"]
                  [org.clojure/test.check "0.9.0"]])
